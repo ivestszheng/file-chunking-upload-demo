9b13a9682bab0fe94cb4c745d589ed14e6829329
无声 <ivestszheng@qq.com>
2022-06-28 22:13:23 +0800

    init

---
+++ server/app.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @Date: 2022-06-28 21:55:20
+ * @LastEditTime: 2022-06-28 21:56:25
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
